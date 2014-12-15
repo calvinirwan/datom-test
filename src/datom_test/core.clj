97dcb4ec75c73b62163dc35f9de2a997deadff5b
calvin irwan <calvin.irwan@gmail.com>
2014-12-15 21:41:22 +0700

    updated schema

--- src/datom_test/core.clj
+++ src/datom_test/core.clj
@@ -7,6 +7,7 @@
 (def uri "datomic:mem://awesome")
 
 (defn create-db [uri] (d/create-database uri))
+(create-db uri)
 (def conn (d/connect uri))
 
 (defn get-like [name](d/q '[:find ?p :in $ ?n :where [?e :person/name ?n]
@@ -19,18 +20,36 @@
      :db/doc
      "hello world"]]))
 
+;;schema question
 [{:db/id #db/id[:db.part/db]
-  :db/ident :person/name
+  :db/ident :question/text
   :db/valueType :db.type/string
   :db/cardinality :db.cardinality/one
   :db.install/_attribute :db.part/db}
  {:db/id #db/id[:db.part/db]
-  :db/ident :person/like
+  :db/ident :question/answer
+  :db/valueType :db.type/ref
+  :db/cardinality :db.cardinality/one
+  :db.install/_attribute :db.part/db}
+ ]
+
+;;schema question answer
+[{:db/id #db/id[:db.part/db]
+  :db/ident :qa/text
   :db/valueType :db.type/string
   :db/cardinality :db.cardinality/one
   :db.install/_attribute :db.part/db}
  {:db/id #db/id[:db.part/db]
-  :db/ident :person/address
+  :db/ident :question/answer
+  :db/valueType :db.type/ref
+  :db/cardinality :db.cardinality/one
+  :db.install/_attribute :db.part/db}
+ ]
+
+;;schema answers
+[{:db/id #db/id[:db.part/db]
+  :db/ident :answer/text
   :db/valueType :db.type/string
   :db/cardinality :db.cardinality/one
-  :db.install/_attribute :db.part/db}]
+  :db.install/_attribute :db.part/db}
+ ]
