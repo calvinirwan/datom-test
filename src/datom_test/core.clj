0444c7688d8be7ee0230e99a0f1e17b0846ffbc4
calvinirwan <calvin.irwan@gmail.com>
2014-12-15 16:02:34 +0700

    schema and first query

--- src/datom_test/core.clj
+++ src/datom_test/core.clj
@@ -7,9 +7,10 @@
 (def uri "datomic:mem://awesome")
 
 (defn create-db [uri] (d/create-database uri))
-
 (def conn (d/connect uri))
 
+(defn get-like [name](d/q '[:find ?p :in $ ?n :where [?e :person/name ?n]
+			[?e :person/like ?p]] (d/db conn) name))
 (def tx-result
   (d/transact
    conn
@@ -18,3 +19,18 @@
      :db/doc
      "hello world"]]))
 
+[{:db/id #db/id[:db.part/db]
+  :db/ident :person/name
+  :db/valueType :db.type/string
+  :db/cardinality :db.cardinality/one
+  :db.install/_attribute :db.part/db}
+ {:db/id #db/id[:db.part/db]
+  :db/ident :person/like
+  :db/valueType :db.type/string
+  :db/cardinality :db.cardinality/one
+  :db.install/_attribute :db.part/db}
+ {:db/id #db/id[:db.part/db]
+  :db/ident :person/address
+  :db/valueType :db.type/string
+  :db/cardinality :db.cardinality/one
+  :db.install/_attribute :db.part/db}]
